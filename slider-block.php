c161f0d8c712ad3c2a70314677532af0e7cf610d
punitverma123 <punitv342@gmail.com>
2024-12-24 08:12:24 +0530

    update function name

--- slider-block.php
+++ slider-block.php
@@ -22,12 +22,10 @@ if (! defined('ABSPATH')) {
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
  * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function create_block_slider_block_block_init()
+function slider_block_init()
 {
 	register_block_type(__DIR__ . '/build/slider');
 	register_block_type(__DIR__ . '/build/slide');
 }
-add_action('init', 'create_block_slider_block_block_init');
+add_action('init', 'slider_block_init');
