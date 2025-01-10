b23019f6b3e69efad260f9cb6a662cd42701efbd
punitverma123 <punitv342@gmail.com>
2025-01-10 16:25:10 +0530

    remove build folder

--- build/slider/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => 'cd486dd3276b27871c1a');
