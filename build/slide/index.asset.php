84bdfb5404d917e92ec84546e4ee1a667b9adbc1
punitverma123 <punitv342@gmail.com>
2025-03-23 17:36:34 +0530

    fix: update asset versions for slider and slide components

--- build/slide/index.asset.php
+++ build/slide/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => 'ff02c0a6f53ce491337f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => 'a533a454279032527c75');
--- build/slider/view.asset.php
+++ build/slider/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '5b0a0c0d395f890ecd99');
+<?php return array('dependencies' => array(), 'version' => '6bdb5d907b4ba2962e12');
