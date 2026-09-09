63d7254e552b7d745b4241d3ab342d3578bafb4b
GitHub Action <action@github.com>
2026-09-09 14:56:44 +0000

    Build and deploy to playground

---
+++ build/slider/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-notices', 'wp-primitives'), 'version' => '612891cd4d9f30668e48');
---
+++ build/slider/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fe890bdf811e2daa49dd');
